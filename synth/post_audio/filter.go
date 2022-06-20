181a84206031fc7abe21059a2079fa837c93e493
hodeiez <hodeinaiz@hotmail.com>
2022-06-21 00:08:59 +0200

    added moog filter

--- synth/post_audio/filter.go
+++ synth/post_audio/filter.go
@@ -24,22 +24,58 @@ func Highpass(fs []float32, freq float64, delay float32, sr float64) []float32 {
 
 	return output
 }
+
+//MOOG FILTER
 func Lowpass(input []float32, freq float64, delay float32, sr float64, resoVal float64) []float32 {
 	output := make([]float32, len(input))
 	copy(output, input)
-	q := float32(resoVal) / 100
-	costh := 2. - math.Cos((tau*freq)/sr)
-	coef := float32(math.Sqrt(costh*costh-1.)) - float32(costh)
+	newF := freq / 5000
+	newR := float32(resoVal / 1000)
+	in1 := float32(0.0)
+	in2 := float32(0.0)
+	in3 := float32(0.0)
+	in4 := float32(0.0)
+	out1 := float32(0.0)
+	out2 := float32(0.0)
+	out3 := float32(0.0)
+	out4 := float32(0.0)
+	f := float32(newF * 1.16)
+	fb := newR * (1 - 0.15*f*f)
 
-	for i, a := range output {
-
-		output[i] = a*(q)*(1+coef) - delay*coef
-		delay = output[i]
+	for i, _ := range output {
+		input[i] -= out4 * fb
+		input[i] *= 0.35013 * (f * f) * (f * f)
+		out1 = input[i] + 0.3*in1 + (1-f)*out1
+		in1 = input[i]
+		out2 = out1 + 0.3*in2 + (1-f)*out2
+		in2 = out1
+		out3 = out2 + 0.3*in3 + (1-f)*out3
+		in3 = out2
+		out4 = out3 + 0.3*in4 + (1-f)*out4
+		in4 = out3
+		output[i] = out4
 
 	}
 
 	return output
 }
+
+// func Lowpass(input []float32, freq float64, delay float32, sr float64, resoVal float64) []float32 {
+// 	output := make([]float32, len(input))
+// 	copy(output, input)
+// 	q := float32(resoVal) / 100
+// 	costh := 2. - math.Cos((tau*freq)/sr)
+// 	coef := float32(math.Sqrt(costh*costh-1.)) - float32(costh)
+
+// 	for i, a := range output {
+
+// 		output[i] = a*(q)*(1+coef) - delay*coef
+// 		delay = output[i]
+
+// 	}
+
+// 	return output
+// }
 
 func Bandpass(input []float32, freq float64, delay float32, sr float64, q float64) []float32 {
 	// 200-300
